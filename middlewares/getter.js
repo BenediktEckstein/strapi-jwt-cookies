3833e90c6598e6f5fd42f2adf8fd19e44d6190fd
Benedikt Eckstein <beckstein@shiftphones.com>
2024-08-02 13:40:22 +0200

    Reduce to query Token

--- middlewares/getter.js
+++ middlewares/getter.js
@@ -5,22 +5,22 @@ const { COOKIE_NAME, payloadOpts } = require('../config')
 
 module.exports = (config, { strapi }) => {
   return async ({ request, cookies }, next) => {
-    if (isFromFrontend(request) && request.url.startsWith('/api')) {
-      const payload = cookies.get(COOKIE_NAME.PAYLOAD)
-      const headersAndSignature = cookies.get(COOKIE_NAME.HEADER_SIGNATURE)
+    if (request.url.startsWith('/api') && !request.headers.authorization) {
+      const jwt = cookies.get('token')
+      // const headersAndSignature = cookies.get('token.sig')
 
       // reconstruct the jwt from the cookies
-      if (payload && headersAndSignature) {
-        const jwt = joinJwt(payload, headersAndSignature)
+      if (jwt) {
+        // const jwt = joinJwt(payload, headersAndSignature)
         request.headers.authorization = `Bearer ${jwt}`
       }
 
       await next()
 
       // renew 'COOKIE_NAME.PAYLOAD' expire time.
-      if (payload && !request.url.startsWith('/api/auth/logout')) {
-        cookies.set(COOKIE_NAME.PAYLOAD, payload, payloadOpts)
-      }
+      // if (payload && !request.url.startsWith('/api/auth/logout')) {
+      //   cookies.set(COOKIE_NAME.PAYLOAD, payload, payloadOpts)
+      // }
     } else {
       await next()
     }
